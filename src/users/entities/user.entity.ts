ce499e060ffc3cd7b7cb49e0ca752f4837b0eab8
maria <airamfdezs@gmailcom>
2022-03-06 21:10:35 +0100

    TypeOrmModule

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,10 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class User{
+    @PrimaryGeneratedColumn()
     id: number;
+    @Column()
     name: string;
+    @Column()
     surname: string;
+    @Column()
     dni: string;
+    @Column()
     email: string;
+    @Column()
     password: string;
+    @Column()
     phone: string;
     
 }
